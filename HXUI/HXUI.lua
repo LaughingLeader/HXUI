c21011f76b430b6a6b859d0e2f5cb421aa86aa8f
David Mitchell <mitch@two-tails.com>
2024-01-08 22:27:34 +0000

    Release script to make releases

--- HXUI/HXUI.lua
+++ HXUI/HXUI.lua
@@ -24,7 +24,7 @@
 
 addon.name      = 'HXUI';
 addon.author    = 'Team HXUI (Tirem, Shuu, colorglut, RheaCloud)';
-addon.version   = '1.1.1';
+addon.version   = '1.1.1-onimitch1';
 addon.desc      = 'Multiple UI elements with manager';
 addon.link      = 'https://github.com/tirem/HXUI'
 
